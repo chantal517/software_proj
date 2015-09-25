2c606065a0d45663bac2381989336d830e7fdaf0
chantal517 <Chantal@mwc-021095.dhcp.missouri.edu>
2015-09-24 20:17:14 -0500

    added repository

---
+++ php/application/views/Instructor_view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>bootstrap/css/bootstrap.css" />
+    <style>
+        body {
+            background: url(http://i.imgur.com/GHr12sH.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+
+<h1> Computer Science Department </h1>
+<h2> Applicants </h2>
+
+
+<?php
+	
+	foreach ($applications as $row){
+		foreach($row as $key => $value){
+		echo "<li>".$key." => ".$value."</li>";
+		}
+		echo"-------------------------------------------------";
+	 	echo "<br>";
+	 	echo "<br>";
+	}
+	
+?>
+
+   
+
+
+
+</body>
+
+</html>
---
+++ php/application/views/pass.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=UTF-8>
+<div align = "center">
+<title>Welcome to apply TA/PLA</title>
+</div>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>bootstrap/css/bootstrap.css" />
+</head>
+<body>
+	<div align = "center">
+		<h1>The time is passed</h1>
+</div>
+</form>
+</body>
+</html>
